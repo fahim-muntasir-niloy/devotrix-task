b5c78b915eb8d7ca2662b6c7d1f5ef7651f051ad
Fahim Muntasir <niloyf3@gmail.com>
2024-08-11 17:35:16 +0600

    ouputs to main elaborated

--- govt_info/src/main.py
+++ govt_info/src/main.py
@@ -3,12 +3,13 @@ from xml_to_pdf import xml_to_pdf
 
 
 
-
 def main(site_link = "https://www.govinfo.gov/bulkdata/CFR/2024/title-12"):
+    
     latest_xml = fetch_latest_xml(site_link)
-    print(latest_xml)
+    print(f"Found latest XML file at: {latest_xml}. Conversion started.")
+    
     xml_to_pdf(site_link, latest_xml)
-    print(f"PDF saved")
+    print(f"{latest_xml.split('/')[-1]} converted to PDF and saved.")
 
 
 
